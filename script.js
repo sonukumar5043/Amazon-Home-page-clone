410cd13e054f4d798d5cc0c61f6b97e3dc67cc52
sonu540 <161741935+sonu540@users.noreply.github.com>
2025-07-21 21:29:35 +0530

    amazone clone

---
+++ script.js
@@ -0,0 +1,5 @@
+const option=document.querySelector("#panoption");
+option.addEventListener("click",(evt)=>{
+    option.innerHTML='<ul><li><a href="#">are</a></li>  <li><a href="#">awho</a></li></ul>';
+    console.log(option);
+})
